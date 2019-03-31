d7c1fbeb03a98f5c934f9b369502187055f80d47
Abdi <abdimohamed862992@gmail.com>
2019-03-31 01:55:58 +0100

    saved

---
+++ creds.js
@@ -0,0 +1,10 @@
+/* eslint-disable quotes */
+const Cred = {
+  user: "serviziowebtorrefazione@gmail.com",
+  clientId:
+    "272405394289-77onl5mb7q13aakqvi0t7cklf797hmrn.apps.googleusercontent.com",
+  clientSecret: "S-SZbRQbX8APRkzhBm3Kwrkq",
+  refreshToken: "1/eMBS8FySymED3hM8vufmjM-QWYPPG-kQC9tfeIc07UA"
+};
+
+module.exports = Cred;
