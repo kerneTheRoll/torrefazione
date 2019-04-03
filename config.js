6684568d593a480f088800cf2b5df726779fd303
Abdi <abdimohamed862992@gmail.com>
2019-04-03 20:33:41 +0200

    compress

--- config.js
+++ config.js
@@ -1,27 +1,29 @@
-var express = require('express'),
-    favicon = require('serve-favicon'),
-    logger = require('morgan'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override'),
-    errorHandler = require('errorhandler'),
-    http = require('http'),
-    path = require('path');
-
-module.exports = function() {
+var express = require("express"),
+  favicon = require("serve-favicon"),
+  logger = require("morgan"),
+  bodyParser = require("body-parser"),
+  methodOverride = require("method-override"),
+  errorHandler = require("errorhandler"),
+  http = require("http"),
+  path = require("path");
+compression = require("compression");
+module.exports = (function() {
   var app = express();
 
   // all environments
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'pug');
+  app.set("port", process.env.PORT || 3000);
+  app.set("views", path.join(__dirname, "views"));
+  app.set("view engine", "pug");
   app.use(favicon("public/images/punch.png"));
-  app.use(logger('dev'));
+  app.use(logger("dev"));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(methodOverride());
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, "public")));
 
+  // compress all responses
+  app.use(compression());
   app.use(errorHandler());
 
   return app;
-}();
+})();
